79de3ff2b8d6e1dd562c5aafa66cd38864937403
Arild <65564205+arildyk@users.noreply.github.com>
2022-02-20 18:44:30 -0500

    Added fonts for styling

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:code_text_field/code_text_field.dart';
 import 'package:flutter/material.dart';
@@ -30,8 +32,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        fontFamily:
-            GoogleFonts.ebGaramond().copyWith(color: Colors.white).fontFamily,
+        fontFamily: 'EBGaramond',
       ),
       home: const MyHomePage(),
       debugShowCheckedModeBanner: false,
@@ -48,16 +49,18 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   CodeController? _codeController;
-  var source = "# simple-markdown-editor\nA Markdown Editor created powered by Java that uses GitHub Markdown flavouring. The most important aspects of this application are the abilities to open a Markdown file, allow the user to edit the file, and finally save the file.\n" +
-      "\n## Built With\n" +
-      "- [commonmark-java](https://github.com/commonmark/commonmark-java)\n" +
+  var source = "# simple-markdown-editor\nA Markdown Editor created powered by Java that uses GitHub Markdown flavouring." +
+      " The most important aspects of this application are the abilities to open a Markdown file, allow the user to edit the file, and finally save the file.\n" +
+      "\n## Built With\n"
+          "- [commonmark-java](https://github.com/commonmark/commonmark-java)\n" +
       "- [JavaFX](https://openjfx.io/)\n" +
       "- [Prism.js](https://prismjs.com/)\n" +
       "- [JSoup](https://jsoup.org/)\n" +
       "- [RichTextFX](https://github.com/FXMisc/RichTextFX)\n" +
       "- [Flying Saucer](https://github.com/flyingsaucerproject/flyingsaucer)\n" +
       "- [iText 2.1.7](https://github.com/hwinkler/itext2)\n\n" +
-      "## Installation\nClone the repository. With JavaFX installed, inside `.vscode`, change the paths to the JavaFX jar files to your own paths in the *settings.json*. In the *launch.json* on the `vmArgs`, change the module path to your own. Run the application with `App.java`.\n\n" +
+      "## Installation\nClone the repository. With JavaFX installed, inside `.vscode`, change the paths to the JavaFX jar files to your own paths in the *settings.json*. " +
+      "In the *launch.json* on the `vmArgs`, change the module path to your own. Run the application with `App.java`.\n\n" +
       "## License\nDistributed under the MIT License. See `LICENSE` for more information.";
 
   @override
@@ -65,14 +68,22 @@ class _MyHomePageState extends State<MyHomePage> {
     super.initState();
     // Instantiate the CodeController
     _codeController = CodeController(
-        text: source,
-        language: markdown,
-        theme: vs2015Theme,
-        onChange: (text) {
-          setState(() {
-            source = text;
-          });
+      text: source,
+      language: markdown,
+      theme: vs2015Theme,
+      onChange: (text) {
+        setState(() {
+          source = text;
         });
+      },
+      patternMap: {
+        r"\B# .+\b|\B## .+\b|\B### .+\b|\B#### .+\b|\B##### .+\b|\B###### .+\b":
+            TextStyle(
+          fontWeight: FontWeight.bold,
+          color: vs2015Theme.values.toList()[2].backgroundColor,
+        ),
+      },
+    );
   }
 
   @override
@@ -100,7 +111,14 @@ class _MyHomePageState extends State<MyHomePage> {
                   Expanded(
                     child: CodeField(
                       controller: _codeController!,
-                      textStyle: GoogleFonts.courierPrime(),
+                      textStyle: const TextStyle(
+                        fontFamily: 'Consolas',
+                      ),
+                      expands: true,
+                      lineNumberStyle: const LineNumberStyle(
+                        width: 58,
+                        margin: 25,
+                      ),
                     ),
                   ),
                 ],
@@ -149,25 +167,55 @@ class _MyHomePageState extends State<MyHomePage> {
                       styleSheet: MarkdownStyleSheet.fromTheme(
                         Theme.of(context),
                       ).copyWith(
-                          h1: const TextStyle(
-                              color: Colors.white, fontWeight: FontWeight.bold),
-                          h2: const TextStyle(
-                              color: Colors.white, fontWeight: FontWeight.bold),
-                          h3: const TextStyle(
-                              color: Colors.white, fontWeight: FontWeight.bold),
-                          h4: const TextStyle(
-                              color: Colors.white, fontWeight: FontWeight.bold),
-                          h5: const TextStyle(
-                              color: Colors.white, fontWeight: FontWeight.bold),
-                          h6: const TextStyle(
-                              color: Colors.white, fontWeight: FontWeight.bold),
-                          p: const TextStyle(color: Colors.white),
-                          code: GoogleFonts.courierPrime().copyWith(
-                              color: Colors.white,
-                              backgroundColor: const Color(0xff595A53)),
-                          codeblockDecoration: BoxDecoration(
-                              color: Color(0xff595A53),
-                              borderRadius: BorderRadius.circular(6))),
+                        h1: const TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 32,
+                        ),
+                        h2: const TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 24,
+                        ),
+                        h3: const TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18.72,
+                        ),
+                        h4: const TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                        h5: const TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 13.28,
+                        ),
+                        h6: const TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 10.72,
+                        ),
+                        p: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                        ),
+                        code: const TextStyle(
+                          color: Colors.white,
+                          backgroundColor: Color(0xff595A53),
+                          fontFamily: 'Consolas',
+                          fontSize: 16,
+                        ),
+                        codeblockDecoration: BoxDecoration(
+                          color: const Color(0xff595A53),
+                          borderRadius: BorderRadius.circular(6),
+                        ),
+                        listBullet: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                        ),
+                      ),
                     ),
                   ),
                 ],
